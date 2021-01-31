05824240fcf7492f0785d86c42ac485b3324a36c
pauljaijai <63136413+pauljaijai@users.noreply.github.com>
2021-02-01 00:12:47 +0800

    Update landing page top service
    The photos of top service were updated

--- src/components/Lpcontent/Lpcontent.js
+++ src/components/Lpcontent/Lpcontent.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './Lpcontent.css';
-import Topservicecard from "./Topservicecard";
 
 
 
@@ -9,15 +8,21 @@ function  Lpcontent(props) {
     
         return(
         <div>
-           
-            <Topservicecard  topservicecat="Beauty" />
-            <Topservicecard topservicecat="Tutor"/>
-            <Topservicecard topservicecat="Personal Trainer"/>
-            <Topservicecard topservicecat="Web developer"/>
-            
-            
-            
+            <div className="contentContainer">
+                <div className="topSertopic">
+                    Top service
+                </div>
+
+                <div className="topSerbox">
+                    <div className="boxPT"><a className="serbot" >Personal Trainer</a></div>
+                    <div className="boxWD"><a className="serbot">Web Developer</a></div>
+                    <div className="boxBB"><a className="serbot">Barber</a></div>
+                    <div className="boxBT"><a className="serbot">Beauty</a></div>
+                </div>
+                
             </div>
+            
+        </div>
             )    
     
 }
