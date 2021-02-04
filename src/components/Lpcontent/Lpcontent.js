a28cfa5ac6f23e741212dae680fa0f1cb18aba58
pauljaijai <63136413+pauljaijai@users.noreply.github.com>
2021-02-04 13:38:03 +0800

    Updated topservice list UI
    I updated the ui with some stupid code. The titles are localed at bottom og the images. Scale up hover effect is deployed

--- src/components/Lpcontent/Lpcontent.js
+++ src/components/Lpcontent/Lpcontent.js
@@ -14,10 +14,10 @@ function  Lpcontent(props) {
                 </div>
 
                 <div className="topSerbox">
-                    <div className="boxPT"><a className="serbot" >Personal Trainer</a></div>
-                    <div className="boxWD"><a className="serbot">Web Developer</a></div>
-                    <div className="boxBB"><a className="serbot">Barber</a></div>
-                    <div className="boxBT"><a className="serbot">Beauty</a></div>
+                    <div className="boxPT"><div className="serbot" >Personal Trainer</div></div>
+                    <div className="boxWD"><div  className="serbot">Web Developer</div></div>
+                    <div className="boxBB"><div  className="serbot">Barber</div></div>
+                    <div className="boxBT"><div className="serbot">Beauty</div></div>
                 </div>
                 
             </div>
